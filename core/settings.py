90487809ec23705e158edc86b13c976750b16c25
zihinbender <73230187+zihinbender@users.noreply.github.com>
2026-01-09 15:43:37 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -136,7 +136,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="sqlite:///db.sqlite3",
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
         ssl_require=False,
     )
